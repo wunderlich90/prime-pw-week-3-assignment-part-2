93003055440797e12f90651ffb0e6eefae4c6c89
Jeremy Cady <jcady90@icloud.com>
2021-06-23 14:19:02 -0500

    finished except 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -68,4 +68,9 @@ while (number <= max) {
 
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
-console.log('count backwards from 10 to 5');
+number = 10;
+let min = 5;
+while (number >= min) {
+  console.log('count backwards from 10 to 5', number);
+  number--;
+}
